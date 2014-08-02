2b97637176fc7b3fdc5f195fb32bed643a81957a
dl <nxycdl@163.com>
2014-08-02 11:52:59 +0800

    20140802

--- www/js/app.js
+++ www/js/app.js
@@ -2,9 +2,8 @@
 'use strict';
 
 var wsUrl = "http://61.133.217.140:808/otrs/nph-genericinterface.pl/Webservice/GenericTicketConnector";
-angular.module('otrsapp', [])
+angular.module('otrsapp', ['ionic', 'm_authservices', 'm_loginControllers'])
     .config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
-
         $stateProvider
             .state('login', {
                 url: "/login",
@@ -16,5 +15,14 @@ angular.module('otrsapp', [])
 .run(function ($rootScope, $window, $location, AuthService) {
     $rootScope.$on("$locationChangeStart", function (event, next, current) {
         console.log('logging ');
+        if (AuthService.isLoggedIn($window)) {
+            //登录
+            //event.preventDefaul t();
+        } else {
+            //未登录
+            alter('a');
+            $location.path('/login');
+            //event.preventDefault();
+        }
     });
 });
