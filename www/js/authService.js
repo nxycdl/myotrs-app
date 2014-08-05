2c75169ea4c00b97affb55497da0a1c603225d2e
nxycdl <nxycdl@163.com>
2014-08-05 23:17:44 +0800

    1

--- www/js/authService.js
+++ www/js/authService.js
@@ -1,15 +1,19 @@
 //Auth服务实现，业务逻辑
 'use strict';
-angular.module('otrsapp.authservices', []).factory('AuthService', function ($q, CommonService) {
-  return {
-    login: function ($http, credentials) {
-      return true;
-    },
-    isLoggedIn: function ($window) {
-      return true;
-    },
-    logout: function ($window) {
-      return true;
+angular.module('otrsapp.authservices', []).factory('AuthService', function ($q, CommServices) {
+    console.log('authservices module');
+    return {
+        login: function ($http, credentials) {
+            console.log('call login');
+            return true;
+        },
+        isLoggedIn: function ($window) {
+            console.log('call is LoggedIn');
+            return true;
+        },
+        logout: function ($window) {
+            console.log('logout');
+            return true;
+        }
     }
-  }
 });
